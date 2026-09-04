7077e6254e317607ed554dddd060bcb20ddd6075
EricLee-02 <1774765937@qq.com>
2026-09-04 15:25:58 +0800

    use cuda device for colab

--- src/train.py
+++ src/train.py
@@ -18,7 +18,7 @@ BATCH_SIZE = 8
 EPOCHS = 100
 LR = 3e-4
 WEIGHT_DECAY = 0.01
-DEVICE = torch.device("cuda" if torch.backends.mps.is_available() else "cpu")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Device:", DEVICE)
 os.makedirs(CHECKPOINT_DIR,exist_ok=True)
 
