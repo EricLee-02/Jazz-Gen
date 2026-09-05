18ca9c0623894e68caedc8f79aca7e20f9e95579
EricLee-02 <1774765937@qq.com>
2026-09-05 17:48:06 +0800

    update generation and midi decoder

--- src/generate.py
+++ src/generate.py
@@ -11,62 +11,33 @@ from Transformer import JazzTransformer
 # =========================
 
 BASE_DIR = "/content/drive/MyDrive/JazzGen_Data"
-
-VOCAB_FILE = (BASE_DIR + "/vocabulary/vocabulary.json"
-)
-
+VOCAB_FILE = (BASE_DIR + "/vocabulary/vocabulary.json")
 CHECKPOINT = (BASE_DIR +"/check_point/best_model.pt")
-
-
 OUTPUT_DIR = (BASE_DIR +"/generated")
-
-
-os.makedirs(
-    OUTPUT_DIR,
-    exist_ok=True
-)
-
-
+os.makedirs(OUTPUT_DIR,exist_ok=True)
 SEQ_LENGTH = 512
-
-MAX_GENERATE_LENGTH = 512
-
-TEMPERATURE = 0.9
-
+MAX_GENERATE_LENGTH = 2048
+TEMPERATURE = 0.8
 TOP_K = 50
 
-
-DEVICE = torch.device(
-    "cuda"
-    if torch.cuda.is_available()
-    else "cpu"
-)
+REPETITION_PENALTY = 1.1
+DEVICE = torch.device("cuda"if torch.cuda.is_available() else "cpu")
 
 
 # =========================
 # Load Vocabulary
 # =========================
 
-with open(
-    VOCAB_FILE,
-    "r"
-) as f:
-
+with open(VOCAB_FILE,"r") as f:
     vocab = json.load(f)
 
-
 token_to_id = vocab["token_to_id"]
-
-id_to_token = {
-    int(k):v
-    for k,v in vocab["id_to_token"].items()
-}
-
+id_to_token = {int(k):v for k,v in vocab["id_to_token"].items()}
 
 BOS_ID = token_to_id["<BOS>"]
-
 EOS_ID = token_to_id["<EOS>"]
-
+PAD_ID = token_to_id["<PAD>"]
+UNK_ID = token_to_id["<UNK>"]
 
 VOCAB_SIZE = len(token_to_id)
 
@@ -85,32 +56,21 @@ model = JazzTransformer(
     dropout=0.1
 )
 
+checkpoint = torch.load(CHECKPOINT,map_location=DEVICE,weights_only=False)
 
-checkpoint = torch.load(
-    CHECKPOINT,
-    map_location=DEVICE
-)
-
-
-model.load_state_dict(
-    checkpoint["model_state_dict"]
-)
-
+model.load_state_dict(checkpoint["model_state_dict"])
 
 model.to(DEVICE)
 
 model.eval()
 
+print("=" * 50)
 
-print(
-    "Loaded checkpoint epoch:",
-    checkpoint["epoch"]
-)
+print("Loaded checkpoint epoch:",checkpoint["epoch"])
 
-print(
-    "Vocabulary:",
-    VOCAB_SIZE
-)
+print("Vocabulary:",VOCAB_SIZE)
+
+print("=" * 50)
 
 
 
@@ -118,36 +78,25 @@ print(
 # Sampling Function
 # =========================
 
-def sample_next_token(logits):
+def sample_next_token(logits,history_tokens):
 
     logits = logits / TEMPERATURE
 
+    logits[PAD_ID] = -float("inf")
+    logits[UNK_ID] = -float("inf")
+
+    recent_tokens = set(history_tokens[-50:])
+
+
+    for token in recent_tokens:
+        logits[token] /= REPETITION_PENALTY
 
     # top-k filtering
+    values, indices = torch.topk(logits,TOP_K)
 
-    values, indices = torch.topk(
-        logits,
-        TOP_K
-    )
-
-
-    probs = F.softmax(
-        values,
-        dim=-1
-    )
-
-
-    next_index = torch.multinomial(
-        probs,
-        num_samples=1
-    )
-
-
-    next_token = indices[
-        next_index
-    ]
-
-
+    probs = F.softmax(values,dim=-1)
+    next_index = torch.multinomial(probs,num_samples=1)
+    next_token = indices[next_index]
     return next_token.item()
 
 
@@ -160,52 +109,16 @@ def sample_next_token(logits):
 @torch.no_grad()
 def generate():
 
-    tokens = [
-        BOS_ID
-    ]
-
-
-    for i in range(
-        MAX_GENERATE_LENGTH
-    ):
-
-        input_ids = torch.tensor(
-            tokens,
-            dtype=torch.long
-        ).unsqueeze(0)
-
-
-        input_ids = input_ids.to(
-            DEVICE
-        )
-
-
-        logits = model(
-            input_ids
-        )
-
-
-        next_logits = logits[
-            0,
-            -1
-        ]
-
-
-        next_token = sample_next_token(
-            next_logits
-        )
-
-
-        tokens.append(
-            next_token
-        )
-
-
+    tokens = [BOS_ID]
+    for step in range(MAX_GENERATE_LENGTH):
+        input_ids = torch.tensor(tokens,dtype=torch.long).unsqueeze(0)
+        input_ids = input_ids.to(DEVICE)
+        logits = model(input_ids)
+        next_logits = logits[0,-1]
+        next_token = sample_next_token(next_logits,tokens)
+        tokens.append(next_token)
         if next_token == EOS_ID:
-            break
-
-
-
+            print("EOS generated at step:", step)
     return tokens
 
 
@@ -216,15 +129,9 @@ def generate():
 
 
 def decode_tokens(tokens):
-
     result=[]
-
     for t in tokens:
-
-        result.append(
-            id_to_token[t]
-        )
-
+        result.append(id_to_token[t])
     return result
 
 
@@ -233,53 +140,15 @@ def decode_tokens(tokens):
 # Main
 # =========================
 
-
 if __name__ == "__main__":
 
-
     generated_tokens = generate()
+    print( "Generated length:",len(generated_tokens))
+    decoded = decode_tokens(generated_tokens)
 
-
-    print(
-        "Generated length:",
-        len(generated_tokens)
-    )
-
-
-    decoded = decode_tokens(
-        generated_tokens
-    )
-
-
-    output_file = (
-        OUTPUT_DIR +
-        "/generated_tokens.json"
-    )
-
-
-    with open(
-        output_file,
-        "w"
-    ) as f:
-
-        json.dump(
-            decoded,
-            f,
-            indent=2
-        )
-
-
-    print(
-        "Saved:",
-        output_file
-    )
-
-
-    print(
-        "\nFirst tokens:"
-    )
-
-
-    print(
-        decoded[:100]
-    )
+    output_file = (OUTPUT_DIR +"/generated_tokens.json")
+    with open(output_file,"w") as f:
+        json.dump(decoded,f,indent=2)
+    print("Saved:",output_file)
+    print("\nFirst tokens:")
+    print(decoded[:100])
--- src/midi_decoder.py
+++ src/midi_decoder.py
@@ -1,7 +1,7 @@
 import json
 import os
 import mido
-from mido import Message, MidiFile, MidiTrack
+from mido import Message, MidiFile, MidiTrack,MetaMessage
 
 
 # =========================
@@ -9,27 +9,13 @@ from mido import Message, MidiFile, MidiTrack
 # =========================
 
 BASE_DIR = "/content/drive/MyDrive/JazzGen_Data"
-
-TOKEN_FILE = (
-    BASE_DIR +
-    "/generated/generated_tokens.json"
-)
-
-
-OUTPUT_FILE = (
-    BASE_DIR +
-    "/generated/generated_jazz.mid"
-)
-
-
+TOKEN_FILE = (BASE_DIR +"/generated/generated_tokens.json")
+OUTPUT_FILE = (BASE_DIR +"/generated/generated_jazz.mid")
 TICKS_PER_BEAT = 480
-
-
 
 # =========================
 # Token Parser
 # =========================
-
 
 def parse_token(token):
 
@@ -40,67 +26,37 @@ def parse_token(token):
     ("type",value)
 
     """
-
     parts = token.split("_")
-
-
     if len(parts) < 2:
         return None,None
-
-
-    key = parts[0].lower()
-
-
+    name = parts[0].lower()
     value = parts[-1]
-
-
     try:
         value = int(value)
     except:
         pass
 
+    return name,value
+    # # Note
+    # if "note" in key:
+    #     return "note", value
 
+    # # pitch
+    # if "pitch" in key:
+    #     return "note", value
 
-    # Note
+    # # velocity
+    # if "velocity" in key:
+    #     return "velocity", value
 
-    if "note" in key:
+    # # duration
+    # if "duration" in key:
+    #     return "duration", value
 
-        return "note", value
-
-
-
-    # pitch
-
-    if "pitch" in key:
-
-        return "note", value
-
-
-
-    # velocity
-
-    if "velocity" in key:
-
-        return "velocity", value
-
-
-
-    # duration
-
-    if "duration" in key:
-
-        return "duration", value
-
-
-
-    # time shift
-
-    if "time" in key:
-
-        return "time", value
-
-
-    return None,None
+    # # time shift
+    # if "time" in key:
+    #     return "time", value
+    # return None,None
 
 
 
@@ -111,159 +67,60 @@ def parse_token(token):
 
 
 def tokens_to_midi(tokens):
-
-
-    mid = MidiFile(
-        ticks_per_beat=TICKS_PER_BEAT
-    )
-
-
+    mid = MidiFile(ticks_per_beat=TICKS_PER_BEAT)
     track = MidiTrack()
-
     mid.tracks.append(track)
-
-
-
     current_velocity = 80
-
     current_duration = 480
-
-
-    current_time = 0
-
-
-
-    active_notes=[]
+    pending_time = 0
+    # current_time = 0
+    # active_notes=[]
 
 
 
     for token in tokens:
-
-
         event,value = parse_token(token)
-
-
         if event is None:
             continue
 
+        if event=="TEMPO":
 
+            if isinstance(value,int):
+                tempo = int(60000000/value)
+                track.append(MetaMessage("set_tempo",tempo=tempo,time=0))
 
         # velocity
-
         if event=="velocity":
-
-            current_velocity = max(
-                1,
-                min(value,127)
-            )
-
-
-
+            velocity = max(1,min(value,127))
         # duration
-
         elif event=="duration":
+            duration=value
 
-            current_duration=value
-
-
+        elif event == "time":
+            pending_time += value
 
         # note
-
         elif event=="note":
-
-
             pitch=value
-
-
-            if not isinstance(
-                pitch,
-                int
-            ):
+            if not isinstance(pitch,int):
                 continue
-
-
-            pitch=max(
-                0,
-                min(
-                    pitch,
-                    127
-                )
-            )
-
-
+            pitch=max(0,min(pitch,127))
             # note on
-
-            track.append(
-                Message(
-                    "note_on",
-                    note=pitch,
-                    velocity=current_velocity,
-                    time=0
-                )
-            )
-
-
+            track.append(Message("note_on",note=pitch,velocity=current_velocity,time=pending_time))
             # note off
-
-            track.append(
-                Message(
-                    "note_off",
-                    note=pitch,
-                    velocity=0,
-                    time=current_duration
-                )
-            )
-
-
-
+            pending_time = 0
+            track.append(Message("note_off",note=pitch,velocity=0,time=current_duration))
         # time shift
-
-        elif event=="time":
-
-            current_time += value
-
-
-
     return mid
-
-
-
-
 # =========================
 # Main
 # =========================
 
 
 if __name__=="__main__":
-
-
-    with open(
-        TOKEN_FILE,
-        "r"
-    ) as f:
-
+    with open(TOKEN_FILE,"r") as f:
         tokens=json.load(f)
-
-
-
-    print(
-        "Tokens:",
-        len(tokens)
-    )
-
-
-
-    midi=tokens_to_midi(
-        tokens
-    )
-
-
-    midi.save(
-        OUTPUT_FILE
-    )
-
-
-    print(
-        "Saved:",
-        OUTPUT_FILE
-    )
+    print("Tokens:",len(tokens))
+    midi=tokens_to_midi(tokens)
+    midi.save(OUTPUT_FILE)
+    print("Saved:",OUTPUT_FILE)
