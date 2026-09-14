01f21474e1e05a774b7a6ad6c0c28a83b01ad900
EricLee-02 <1774765937@qq.com>
2026-09-14 16:27:16 +0800

    Update

--- src/config.py
+++ src/config.py
@@ -28,9 +28,9 @@ MELODY_VOCAB_FILE = os.path.join(BASE_DIR,"Melody_Vocabulary","vocabulary.json")
 # ==========================
 
 CHECKPOINT_DIR = os.path.join(BASE_DIR,"Checkpoints")
-HARMONY_CHECKPOINT_DIR = os.path.join(CHECKPOINT_DIR,"Checkpoints","Harmony","harmony_best.pt")
-MELODY_CHECKPOINT_DIR = os.path.join(CHECKPOINT_DIR,"Checkpoints","Melody")
-GENERATION_CHECKPOINT_DIR = os.path.join(CHECKPOINT_DIR,"Checkpoints","Generation")
+HARMONY_CHECKPOINT_DIR = os.path.join(CHECKPOINT_DIR,"Harmony","harmony_best.pt")
+MELODY_CHECKPOINT_DIR = os.path.join(CHECKPOINT_DIR,"Melody")
+GENERATION_CHECKPOINT_DIR = os.path.join(CHECKPOINT_DIR,"Generation")
 # ==========================
 # Output
 # ==========================
